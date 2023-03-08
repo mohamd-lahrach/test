5f9af7b5f5c435e1a6b5a15de08539151ea400e6
mohamd lahrach <mohamedlahrach00@gmail.com>
2023-03-08 11:24:30 +0100

    added

--- app/src/main/java/com/lahrachtech/textinputlayout/Branche.kt
+++ app/src/main/java/com/lahrachtech/textinputlayout/Branche.kt
@@ -6,26 +6,28 @@ class Branche {
     lateinit var id: String
     lateinit var brancheName: String
     lateinit var acronym: String
-    lateinit var years:ArrayList<String>
-    lateinit var teachers:ArrayList<Teacher>
+    lateinit var years: ArrayList<String>
+    lateinit var teachers: ArrayList<Teacher>
 
-    constructor(){
-        this.teachers= arrayListOf()
+    constructor() {
+        this.teachers = arrayListOf()
     }
 
-    constructor(_id: String, _branchName: String, _acronym: String,) {
+    constructor(_id: String, _branchName: String, _acronym: String) {
         this.id = _id
         this.brancheName = _branchName
         this.acronym = _acronym
-        this.years= arrayListOf("first","second")
-        this.teachers= arrayListOf()
+        this.years = arrayListOf("first", "second")
+        this.teachers = arrayListOf()
 
     }
-    fun addTeacher(t:Teacher){
-    this.teachers.add(t)
+
+    fun addTeacher(t: Teacher) {
+        this.teachers.add(t)
         val branchRef = FirebaseDatabase.getInstance().getReference("branches/$id")
         branchRef.child("teachers").setValue(teachers)
     }
+
     override fun toString(): String {
         return "Branche(id='$id', brancheName='$brancheName', acronym='$acronym')"
     }
